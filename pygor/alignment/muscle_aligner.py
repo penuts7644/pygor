5526bd8cbfceb508dcd9984eb3068e697764cdf1
Wout van Helvoirt <wout.van.helvoirt@icloud.com>
2018-10-11 16:32:38 +0200

    MuscleAligner refactor.

--- pygor/alignment/muscle_aligner.py
+++ pygor/alignment/muscle_aligner.py
@@ -14,3 +14,58 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""MuscleAligner class for performing MUSCLE alignments."""
+
+
+from Bio.Align.Applications import MuscleCommandline
+from StringIO import StringIO
+from Bio import AlignIO
+
+
+class MuscleAligner:
+    """Class to perform alignments with MUSCLE via biopython's commandline tool.
+
+    Parameters
+    ----------
+    fasta_file : string
+        A file path to a fasta file containining the genomic data to align.
+
+    """
+    def __init__(self, fasta_file):
+        super(MuscleAligner, self).__init__()
+        self.fasta_file = fasta_file
+        self.alignment = None
+
+    def get_muscle_alignment(self):
+        """Getter function for collecting the MUSCLE alignment.
+
+        Returns
+        -------
+        Bio.AlignIO
+            A biopython alignment object from the fasta file.
+
+        """
+        return self.alignment
+
+    def align(self):
+        """Uses MUSCLE via commandline to create a multi-alignment from fasta.
+
+        Notes
+        -----
+            This function uses the given fasta file for creating an alignment.
+
+        """
+        muscle_cline = MuscleCommandline(input=self.fasta_file)
+        stdout, stderr = muscle_cline()
+        self.alignment = AlignIO.read(StringIO(stdout), "fasta")
+
+
+def main():
+    """Function to be called when file executed via terminal."""
+    print(__doc__)
+
+
+if __name__ == "__main__":
+    main()
