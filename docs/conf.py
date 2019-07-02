e09414b2d6b25c05a34d0c9e6ab93e288f2326d5
Wout van Helvoirt <wout.van.helvoirt@icloud.com>
2019-07-02 22:27:30 +0200

    version bump to 0.1.17

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = u'2019, Wout van Helvoirt'
 author = u'Wout van Helvoirt'
 
 # The short X.Y version
-version = u'0.1.16'
+version = u'0.1.17'
 # The full version, including alpha/beta/rc tags
-release = u'0.1.16'
+release = u'0.1.17'
 
 
 # -- General configuration ---------------------------------------------------
